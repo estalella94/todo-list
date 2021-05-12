a69fd45bce0de0e14c18d63b9a01703014269715
estalella94 <marc.estalella64@gmail.com>
2021-05-12 15:53:06 +0200

    organización de la caja

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+/*------------*/
+//1.REQUSITOS
+/*------------*/
+
+// 1.1 Petición de funcionalidad (reunión 1-2 h con jefe de negocio, de servicios, product owner o project manager)
+//tip: apuntar todo en la reunión
+
+// -> necessitamos una web para almacenar las tareas de los usuarios. API, tematización...
+
+//1.2 traducir a Funcionalidades de HTML, JS , parted de la aplicación
+// - Input con nombre de tarea
+// - Botón para agregar tarea 
+// - Listado de tareas
+// - Botón de eliminar tareas
+// - Botón eliminar tareas individuales
+
+//1.3 Definir acciones de usuario (inputs del programa)
+// - Agragar tarea
+// - Eliminar (tarea o tareas)
+// - Mostrar por pantalla 
+
+//1.4 Definir resultado a mostrar (API + acciones usuario -> output)
+// -Lista de tareas
+
+
+
+/*-----------------*/
+//2.PLANIFICACIÓN
+/*-----------------*/
+
+// 2.1 Hitos de la aplicación 
+// - Crear un HTML estático
+// - Montar una estructura del javascript -> estructura & pseudocódigo
+// - Implementar lógica JS para que sea dinámico
+// - Rellenar las diferentes partes del JS
+// - Linkar &degugar JS + HTML
+// - Mejorar maquetación
+
+//2.2 Tareas concretas -> ganar focus
+// - [HTML] Crear Input y button boostrap y dejar espacio para el listado <ul> [1h]
+// - [JS] Definir las variables que necesitará el programa
+// - [JS] Escribir todas las funciones vacias de javaScript
+// - [JS] Rellenar lógica de las funciones una a una -> probar -> siguiente
+// - [JS] Mostrar al usuario el listado de tareas
+// - [HTML] Mejorari interficie
+
